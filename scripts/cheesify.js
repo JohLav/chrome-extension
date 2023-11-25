4e38d483d50ce635c52b7379c075b746c94e825f
Johana Lavigne <johana.lavigne@gmail.com>
2023-11-25 11:58:34 +0100

    you've been cheesified ; :qa

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,6 +1,14 @@
 function cheesify() {
-  // TODO: Add the image replacement script here
+  // Image replacement script
+  document.querySelectorAll('img').forEach((img) => {
+    img.src = `https://images.unsplash.com/photo-1631379578550-7038263db699?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+    img.srcset = img.src;
+  })
 }
 
-// TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-  
+// Function to listen for messages on the content page using chrome.runtime.onMessage
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message === "Cheesify") {
+    cheesify();
+  }
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,16 @@
 function sendCheesifyMsg() {
+  console.log("J'ai appelé ma fonction");
+  chrome.tabs.query({active:true, currentWindow: true}, function (tabs) {
+    console.log(tabs);
+    chrome.tabs.sendMessage(tabs[0].id, "Cheesify");
+  });
   // TODO: Write a function to send a message to the active tab to 'cheesify' it
 }
 
+const btn = document.querySelector("button");
+console.log(btn);
+
+btn.addEventListener("click", () => {
+  sendCheesifyMsg();
+})
 // TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
